8731d67b97b17fba4544b1b454af6f2256c1078a
amitatiwari255 <amita.tiwari255@gmail.com>
2022-11-28 11:23:31 +0530

    Added: Start Position

---
+++ StartingPosition.java
@@ -0,0 +1,8 @@
+	//Snake and Laddergame played with single player at start position 0
+public class StartingPosition {
+	public static void main (String[] args) 
+	{
+          int playerstartposition = 0 ;
+          System.out.println("Player Start Position Is :" +playerstartposition);
+     }
+}
